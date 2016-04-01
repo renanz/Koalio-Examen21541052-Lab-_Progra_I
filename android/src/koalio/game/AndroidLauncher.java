2f175f78b6fd273f067878ae9aef5a7689ab788b
renanz <renan_zelaya@hotmail.com>
2016-03-31 20:54:05 -0600

    Examen 2, 21541052, Koalio

---
+++ android/src/koalio/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package koalio.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import koalio.game.Koalio;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Koalio(), config);
+	}
+}
---
+++ desktop/src/koalio/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package koalio.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import koalio.game.Koalio;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Koalio(), config);
+	}
+}
